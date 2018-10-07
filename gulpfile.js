f63de3fcaa73d9bd0a4a5433c4d1b1411ce9b211
Eugene Arshinov <earshinov@gmail.com>
2018-10-07 15:30:48 +0300

    Switch to Webpack 3.x from Browserify

--- gulpfile.js
+++ gulpfile.js
@@ -1,67 +1,124 @@
 /* jshint node: true */
 
-var browserify = require("browserify");
+var path = require("path");
+
 var scss = require("gulp-sass");
 var gutil = require("gulp-util");
 var zip = require("gulp-zip");
 var gulp = require("gulp");
 var multipipe = require("multipipe");
 var through2 = require("through2");
-var source = require("vinyl-source-stream");
-var watchify = require("watchify");
+var webpack = require("webpack-stream");
 
-var browserifyOpts = {
-	entries: ["node_modules/app/main.js"],
-	debug: true,
-	verbose: true,
-	transform: [["babelify", { presets: ["es2015"] }]],
-};
+// ========================================================
+// Helpers - logging
+// ========================================================
 
 function logDest() {
 	return through2.obj(function(file, enc, cb) {
-		console.log("-> " + file.path);
+		log("-> " + file.path);
 		cb();
 	});
+}
+
+function log() {
+	gutil.log.apply(gutil, arguments);
+}
+
+function logError(error) {
+	var message = error.messageFormatted || error.message;
+	log(message);
+}
+
+// ========================================================
+// Helpers - streams and error handling
+// ========================================================
+
+function prepStream(s /* : stream */) {
+	return s.on("error", handleError);
 }
 
 function handleError(error) {
 	log(formatError(error));
 	this.end();
 }
-function log() {
-	gutil.log.apply(gutil, arguments);
-}
+
 function formatError(error) {
 	return error.messageFormatted || error.message;
 }
 
-function processJs() {
-	var stream = source("app.js");
-	multipipe(
-		stream,
-		gulp.dest("."),
-		logDest()
-	).on("error", handleError);
-	return stream;
-}
-function buildJs() {
-	browserify(browserifyOpts).bundle()
-		.pipe(processJs());
-}
-function watchJs() {
-	var b = watchify(browserify(Object.assign({}, watchify.args, browserifyOpts)));
-	b.on("update", bundle);
-	function bundle() {
-		return b.bundle()
-			.on("error", function(error) {
-				log("Browserify error:", formatError(error));
-			})
-			.pipe(processJs());
+function prepWebpackStream(s /* : stream */) {
+	//
+	// Обрабатываем события как "error", так и "compilation-error".
+	// По какой-то причине webpack-stream в режиме watch при пересборках выдаёт только последний:
+	//
+	//    if (!options.watch) {
+	// 	    self.emit('error', compilationError);
+	//    }
+	//    self.emit('compilation-error', compilationError);
+	//
+	// В то же время мы не хотим пропустить и error, если там не ошибка компиляции, а что-то другое.
+	//
+	var lastError = null;
+	function handleWebpackError(error) {
+		if (error !== lastError) {
+			logError(error);
+			lastError = error;
+		}
+		//this.end() - omitted intentionally
 	}
-	return bundle();
+	return s
+		.on("error", handleWebpackError)
+		.on("compilation-error", handleWebpackError);
 }
-gulp.task("watch-js", watchJs);
-gulp.task("build-js", buildJs);
+
+// ========================================================
+// JS
+// ========================================================
+
+var WEBPACK_CONFIG = {
+	devtool: "source-map",
+	resolve: {
+		alias: {
+			"app": path.resolve(__dirname, "app")
+		}
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['@babel/preset-env'],
+					}
+				}
+			},
+		]
+	},
+	output: {
+		filename: "app.js",
+	},
+};
+
+function buildJs(/* optional */ webpackStreamConfig) {
+	return function() {
+		return prepStream(gulp.src(["app/main.js"], { base: "." }))
+			.pipe(prepWebpackStream(webpack(Object.assign({
+				config: WEBPACK_CONFIG,
+				quiet: true,
+			}, webpackStreamConfig))))
+			.pipe(prepStream(gulp.dest(".")))
+			.pipe(prepStream(logDest()));
+	};
+}
+gulp.task("watch-js", buildJs({ watch: true }));
+gulp.task("build-js", buildJs());
+
+// ========================================================
+// CSS
+// ========================================================
 
 function buildCss() {
 	return multipipe(
@@ -76,6 +133,8 @@ function watchCss() {
 }
 gulp.task("build-css", buildCss);
 gulp.task("watch-css", watchCss);
+
+// ========================================================
 
 gulp.task("build", ["build-js", "build-css"]);
 gulp.task("dev", ["watch-js", "watch-css"]);
